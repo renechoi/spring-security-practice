c04d3113d6ca437302a024c1c4a16dad4b086dfe
renechoi <renechoi90@gmail.com>
2023-04-01 11:36:19 +0900

    feat: implement notice

---
+++ src/main/java/fast/springsecuritypractice/notice/NoticeRepository.java
@@ -0,0 +1,6 @@
+package fast.springsecuritypractice.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
